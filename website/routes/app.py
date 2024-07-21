db66444531be3125a31e0b9018186eb90a56a6ea
Krunal Patel <ktpatel5999@gmail.com>
2024-07-20 21:46:40 -0400

    Update app.py

--- website/routes/app.py
+++ website/routes/app.py
@@ -1,14 +1,12 @@
 from flask import Flask
-from routes.home import home_bp
-from routes.about import about_bp
-from routes.data import data_bp
+from home import home
+from about import about
+from data import data
 
 app = Flask(__name__)
 
-# Register the blueprints
-app.register_blueprint(home_bp)
-app.register_blueprint(about_bp)
-app.register_blueprint(data_bp)
+app.add_url_rule('/', 'home', home)
+app.add_url_rule('/about', 'about', about)
+app.add_url_rule('/data', 'data', data)
 
-if __name__ == '__main__':
-    app.run(host='localhost', port=9000, debug=True)
+run_simple('localhost',8080,app,use_reloader=False, use_debugger=False)
